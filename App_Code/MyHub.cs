da397a285bd06b62b07042a5b0651bcfc7e0ae69
Sean Hsieh <sean.hsieh@garmin.com>
2017-12-29 14:34:14 +0800

    Create a signalR example

---
+++ App_Code/MyHub.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNet.SignalR;
+using Microsoft.AspNet.SignalR.Hubs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+/// <summary>
+/// Summary description for MyHub
+/// </summary>
+
+[HubName("myhub")]
+public class MyHub:Hub
+{
+    public void serverMethod(string msg)
+    {
+        Clients.All.clientMethod(msg + " good morning");
+        //return msg + " good morning";
+    }
+    public MyHub()
+    {
+        //
+        // TODO: Add constructor logic here
+        //
+    }
+}

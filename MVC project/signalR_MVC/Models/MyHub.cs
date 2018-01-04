aff6b18464ddfa6d496f31ed83f7128fdf36f6d4
Sean Hsieh <sean.hsieh@garmin.com>
2018-01-04 17:15:58 +0800

    Create a MVC project

---
+++ MVC project/signalR_MVC/Models/MyHub.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.AspNet.SignalR;
+using Microsoft.AspNet.SignalR.Hubs;
+
+namespace signalR_MVC.Models
+{
+    [HubName("myhub")]
+    public class MyHub: Hub
+    {
+    }
+}
---
+++ MVC project/signalR_MVC/Startup1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartup(typeof(signalR_MVC.Startup1))]
+
+namespace signalR_MVC
+{
+    public class Startup1
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            app.MapSignalR();
+            // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=316888
+        }
+    }
+}

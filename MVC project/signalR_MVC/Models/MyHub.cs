25595177a79dc3d162bf503331ca9fe8c6fe505e
Sean Hsieh <sean.hsieh@garmin.com>
2018-01-05 11:46:50 +0800

    OnConnected

--- MVC project/signalR_MVC/Models/MyHub.cs
+++ MVC project/signalR_MVC/Models/MyHub.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading.Tasks;
 using System.Web;
 using Microsoft.AspNet.SignalR;
 using Microsoft.AspNet.SignalR.Hubs;
@@ -10,6 +11,15 @@ namespace signalR_MVC.Models
     [HubName("myhub")]
     public class MyHub: Hub
     {
+        public override Task OnConnected()
+        {
+            return Clients.All.log("connected" + DateTime.Now.ToString());
+        }
+        public override Task OnDisconnected(bool stopCalled)
+        {
+            return Clients.All.log("Disconnected" + DateTime.Now.ToString());
+        }
+
         public void servermethod(string name, string msg)
         {
             // call the clients connected to the hub

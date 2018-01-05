ee32534c0fb673ee32b3b2e7830fdccb88ae1aad
Sean Hsieh <sean.hsieh@garmin.com>
2018-01-05 16:09:01 +0800

    ContextObject And JSON Serialization

--- MVC project/signalR_MVC/Models/MyHub.cs
+++ MVC project/signalR_MVC/Models/MyHub.cs
@@ -8,12 +8,30 @@ using Microsoft.AspNet.SignalR.Hubs;
 
 namespace signalR_MVC.Models
 {
+    class Info
+    {
+        public string conId { get; set; }
+        public string  conStatus { get; set; }
+        public string transport { get; set; }
+        public string host { get; set; }
+        public string port { get; set; }
+        public string uname { get; set; }
+    }
+
     [HubName("myhub")]
     public class MyHub: Hub
     {
         public override Task OnConnected()
         {
-            return Clients.All.log("connected" + DateTime.Now.ToString());
+            Info i = new Info();
+            i.conId = Context.ConnectionId.ToString();
+            i.conStatus = Context.Headers["Connection"].ToString();
+            i.transport = Context.QueryString["transport"];
+            i.host = Context.Request.Url.Host;
+            i.port = Context.Request.Url.Port.ToString();
+            i.uname = Context.User.Identity.Name;
+
+            return Clients.All.log(i);
         }
         public override Task OnDisconnected(bool stopCalled)
         {
@@ -22,8 +40,9 @@ namespace signalR_MVC.Models
 
         public void servermethod(string name, string msg)
         {
+            string ConId = Context.ConnectionId.ToString();
             // call the clients connected to the hub
-            Clients.All.clientmethod(name, msg);
+            Clients.All.clientmethod(name, ConId, msg);
         }
     }
 }
